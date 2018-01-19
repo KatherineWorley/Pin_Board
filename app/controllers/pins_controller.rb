a0327ef2b34cd4508a89add9a19fbd6136b5d7e6
Katherine Worley <kdesign216@yahoo.com>
2018-01-19 06:22:05 -0500

    Installed Simple Form, added Views

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,2 +1,17 @@
 class PinsController < ApplicationController
+	def index
+	end
+
+	def new
+		@pin = Pin.new
+	end
+
+	def create
+		@pin = Pin.new(pin.params)
+	end
+
+	private
+	def pin_params
+		params.require(:pin).permit(:title, :description)
+	end
 end
