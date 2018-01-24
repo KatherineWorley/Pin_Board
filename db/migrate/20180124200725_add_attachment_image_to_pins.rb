4c900591b957f9b7f6093868d5ae670711060e72
Katherine Worley <kdesign216@yahoo.com>
2018-01-24 15:14:22 -0500

    updated migration to specify the Rails release

--- db/migrate/20180124200725_add_attachment_image_to_pins.rb
+++ db/migrate/20180124200725_add_attachment_image_to_pins.rb
@@ -1,4 +1,4 @@
-class AddAttachmentImageToPins < ActiveRecord::Migration
+class AddAttachmentImageToPins < ActiveRecord::Migration[5.1]
   def self.up
     change_table :pins do |t|
       t.attachment :image
